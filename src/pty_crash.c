cab9e838c957aeeef81dbf5a0aef070069d72057
zhangxiaosa <15927119728@163.com>
2020-04-01 17:50:53 -0500

    bus for mac

--- src/pty_crash.c
+++ src/pty_crash.c
@@ -13,14 +13,29 @@ int main( int argc, char *argv[] )
 		if(strcmp(buf, "quit") == 0) {
 			return 0;
 		}
-		else if(strcmp(buf, "bus") == 0) {
+		else if(strcmp(buf, "bus_mac") == 0) {
 			char* filename = "hahaha";
 			filename[0] = 'a';
+
+			// int *iptr;
+			// char *cptr;
+			// cptr = malloc(sizeof(int) + 1);
+			// iptr = (int *) ++cptr;
+			// *iptr = 42;
+
+			// unsigned char data[6];
+			// (unsigned int *) (data + 2) = 0xdeadf00d;
+
+			// char buf[3];
+			// sprintf(buf,"123456\n");
+			// short array[16];
+			// int * p = (int *) &array[1];
+			// *p = 1;
 		}
 		else if(strcmp(buf, "segment") == 0) {
 			// int arr[2]; 
 			// arr[3] = 10;
-			int *p; 
+			int *p = 0; 
 			printf("%d",*p); 
 		}
 		else {
