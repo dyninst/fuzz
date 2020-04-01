99f01a8ccaa3561ce3d1fa2bfa903a5754a5dd2d
Mengxiao Zhang <mengxiao_zhang@royal-07.cs.wisc.edu>
2020-04-01 17:55:34 -0500

    did not find bus for linux

--- src/pty_crash.c
+++ src/pty_crash.c
@@ -33,6 +33,25 @@ int main( int argc, char *argv[] )
 			// int * p = (int *) &array[1];
 			// *p = 1;
 		}
+		else if(strcmp(buf, "bus_linux") == 0) {
+			//char* filename = "hahaha";
+			//filename[0] = 'a';
+
+			int *iptr;
+			char *cptr;
+			cptr = malloc(sizeof(int) + 1);
+			iptr = (int *) ++cptr;
+			*iptr = 42;
+
+			//unsigned char data[6];
+			//(unsigned int *) (data + 2) = 0xdeadf00d;
+
+			 char buf[3];
+			 sprintf(buf,"123456\n");
+			 short array[16];
+			 int * p = (int *) &array[1];
+			 *p = 1;
+		}
 		else if(strcmp(buf, "segment") == 0) {
 			// int arr[2]; 
 			// arr[3] = 10;
