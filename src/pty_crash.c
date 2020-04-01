e815b16ebbbb6d1c78344e873b7d25ef0ff4480a
zhangxiaosa <15927119728@163.com>
2020-04-01 16:57:30 -0500

    add a pty_crash.c file

---
+++ src/pty_crash.c
@@ -0,0 +1,30 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <sys/types.h>
+// # include <linux/limits.h>
+int main( int argc, char *argv[] )
+{
+
+	char buf[100];
+
+	while(scanf("%s", buf)) {
+		if(strcmp(buf, "quit") == 0) {
+			return 0;
+		}
+		else if(strcmp(buf, "bus") == 0) {
+			char* filename = "hahaha";
+			filename[0] = 'a';
+		}
+		else if(strcmp(buf, "segment") == 0) {
+			// int arr[2]; 
+			// arr[3] = 10;
+			int *p; 
+			printf("%d",*p); 
+		}
+		else {
+			printf("buf is %s\n", buf);
+		}
+	}
+}
